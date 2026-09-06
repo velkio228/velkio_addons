4868871ec7465079e4ebdded4d8f115737cbce35
vignesh-ck <vignesh-ck@vignesh-ck-Inspiron-3501>
2026-09-06 11:14:34 +0530

    PRICE CHANGE

--- velkio_woocommerce_connector/__manifest__.py
+++ velkio_woocommerce_connector/__manifest__.py
@@ -15,7 +15,7 @@ sync logs, and a backend dashboard for operational visibility.
     "website": "https://apps.odoo.com/apps/modules/browse?author=Velkio%20-%20Odoo%20Solutions",
     "support": "velkio.odoosolution@gmail.com",
     "license": "OPL-1",
-    'price': 325.00,
+    'price': 22.22,
     'currency': 'USD',
     "depends": ["base", "sale_management", "stock", "web"],
     "data": [
